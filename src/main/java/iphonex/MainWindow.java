629b9d4663cd1c068af38e01c70b5625aca9fe1a
godigmh <280139726@qq.com>
2017-10-04 16:11:34 +0800

    购买流程走通

--- src/main/java/iphonex/MainWindow.java
+++ src/main/java/iphonex/MainWindow.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.security.MessageDigest;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -26,6 +28,8 @@ public class MainWindow {
     private JFrame frame;
     private JTextField yzm;
     private OkHttpClient client = new OkHttpClient();
+    private String cartCode;
+    private String addressId;
 
     /**
      * Launch the application.
@@ -64,8 +68,8 @@ public class MainWindow {
         frame.getContentPane().add(yzm);
         yzm.setColumns(10);
         
-        JButton btnNewButton = new JButton("验证码");
-        btnNewButton.addActionListener(new ActionListener() {
+        JButton yzmButton = new JButton("验证码");
+        yzmButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 
 //                Request.Builder requestBuilder = new Request.Builder().url("http://www.baidu.com");
@@ -99,11 +103,11 @@ public class MainWindow {
                 
             }
         });
-        btnNewButton.setBounds(145, 46, 93, 23);
-        frame.getContentPane().add(btnNewButton);
+        yzmButton.setBounds(145, 46, 93, 23);
+        frame.getContentPane().add(yzmButton);
         
-        JButton btnNewButton_1 = new JButton("登陆");
-        btnNewButton_1.addActionListener(new ActionListener() {
+        JButton loginButton = new JButton("登陆");
+        loginButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 
                 SendMsgVO sendMsgVO = new SendMsgVO();
@@ -134,16 +138,14 @@ public class MainWindow {
                 });
             }
         });
-        btnNewButton_1.setBounds(273, 46, 93, 23);
-        frame.getContentPane().add(btnNewButton_1);
+        loginButton.setBounds(273, 46, 93, 23);
+        frame.getContentPane().add(loginButton);
         
         JButton buyButton = new JButton("购买");
         buyButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String sku = "1040095";
-                String goodsId = "1045210";
-//                String json = "sku[0][ModelId]=" + sku + "&sku[0][GoodsId]=" + goodsId + "&sku[0][Num]=1&sku[0][Channel]=1&sku[0][ProvinceId]=200&sku[0][CityId]=200";
-//                RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json);
+                String sku = "1040110";
+                String goodsId = "1045212";
                 FormBody formBody = new FormBody.Builder()
                         .add("sku[0][ModelId]", sku)
                         .add("sku[0][GoodsId]", goodsId)
@@ -153,10 +155,10 @@ public class MainWindow {
                         .add("sku[0][CityId]", "200")
                         .build();
                 Builder builder = new Request.Builder()
-                        .addHeader("cookie", "is_login=true;userinfokey=%7b%22loginType%22%3a%2201%22%2c%22provinceName%22%3a%22200%22%2c%22pwdType%22%3a%2202%22%7d;CmLocation=200|200;PHPSESSID=q8pl44pc3er70qgquvnbi5p5f6")
+                        .addHeader("cookie", "PHPSESSID=q8pl44pc3er70qgquvnbi5p5f6")
                         .addHeader("Referer", "http://touch.10086.cn/goods/200_200_1045210_1040095.html")
                         .addHeader("Origin", "http://touch.10086.cn")
-                        .addHeader("X-Requested-With", "com.greenpoint.android.mc10086.activity")
+//                        .addHeader("X-Requested-With", "com.greenpoint.android.mc10086.activity")
                         .addHeader("User-Agent", "Mozilla/5.0 (Linux; Android 5.1; OPPO A37m Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 leadeon/4.1.0")
                         .url("http://touch.10086.cn/ajax/buy/buy.json")
                         .post(formBody);
@@ -164,23 +166,94 @@ public class MainWindow {
                
                 Call call= client.newCall(request);
                 call.enqueue(new Callback() {
-                    
                     @Override
                     public void onResponse(Call call, Response response) throws IOException {
                         String str = response.body().string();
                         System.out.println(str);
+                        Matcher cartCodeMatcher = Pattern.compile("(?<=cart_code=).+(?=\")").matcher(str);
+                        cartCodeMatcher.find();
+                        cartCode = cartCodeMatcher.group();
+                        System.out.println("cartCode=" + cartCode);
                     }
-                    
                     @Override
                     public void onFailure(Call call, IOException e) {
                         System.out.println("购买");
                     }
                 });
-                
-                
             }
         });
         buyButton.setBounds(375, 46, 93, 23);
         frame.getContentPane().add(buyButton);
+        
+        JButton checkOrderButton = new JButton("确认订单");
+        checkOrderButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Request request = new Request.Builder()
+                        .url("http://touch.10086.cn/order/checkorder.php?cart_code=" + cartCode)
+                        .addHeader("cookie", "PHPSESSID=q8pl44pc3er70qgquvnbi5p5f6")
+                        .addHeader("Referer", "http://touch.10086.cn/goods/200_200_1045212_1040110.html")
+                        .addHeader("User-Agent", "Mozilla/5.0 (Linux; Android 5.1; OPPO A37m Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 leadeon/4.1.0")
+                        .build();
+                Call call= client.newCall(request);
+                call.enqueue(new Callback() {
+                    
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        String str = response.body().string();
+                        Matcher addressIdMatcher = Pattern.compile("(?<=name=\"address_id\" value=\").+(?=\")").matcher(str);
+                        addressIdMatcher.find();
+                        addressId = addressIdMatcher.group();
+                        System.out.println("addressId=" + addressId);
+                    }
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        System.out.println("确认订单失败");
+                    }
+                });
+                
+            }
+        });
+        checkOrderButton.setBounds(488, 46, 93, 23);
+        frame.getContentPane().add(checkOrderButton);
+        
+        JButton addOrderButton = new JButton("添加订单");
+        addOrderButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                FormBody formBody = new FormBody.Builder()
+                        .add("address_id", addressId)
+                        .add("invoice_id", "personal")
+                        .add("pay_type", "1")
+                        .add("coupon_type", "")
+                        .add("eticket_number_mall", "")
+                        .add("cart_code", cartCode)
+                        .add("ticket_no", "")
+                        .add("cart_code", cartCode)
+                        .build();
+                Builder builder = new Request.Builder()
+                        .addHeader("cookie", "PHPSESSID=q8pl44pc3er70qgquvnbi5p5f6")
+                        .addHeader("Referer", "http://touch.10086.cn/order/checkorder.php?cart_code=" + cartCode)
+                        .addHeader("Origin", "http://touch.10086.cn")
+//                        .addHeader("X-Requested-With", "com.greenpoint.android.mc10086.activity")
+                        .addHeader("User-Agent", "Mozilla/5.0 (Linux; Android 5.1; OPPO A37m Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 leadeon/4.1.0")
+                        .url("http://touch.10086.cn/ajax/submitorder/addorder.json")
+                        .post(formBody);
+                Request request = builder.build();
+                Call call= client.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        String str = response.body().string();
+                        System.out.println(str);
+                    }
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        System.out.println("添加订单失败");
+                    }
+                });
+                
+            }
+        });
+        addOrderButton.setBounds(591, 46, 93, 23);
+        frame.getContentPane().add(addOrderButton);
     }
 }
