2bfb312e21994ce0b8683510de4ccb7c76b22083
godigmh <280139726@qq.com>
2018-02-06 20:11:42 +0800

    no matter what, keep committing is always good, isn't it?

---
+++ src/main/java/iphonex/Client.java
@@ -0,0 +1,30 @@
+package iphonex;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+import okhttp3.OkHttpClient;
+
+
+/**
+ * 
+* @ClassName: Client 
+* @Description: 不管怎么样,提交一下总是好的,对吧
+* @author god
+* @date 2018年2月6日 下午8:09:00
+ */
+public enum Client{
+    INSTANCE;
+    private OkHttpClient client;
+    private Client(){
+        client = new OkHttpClient().newBuilder().hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        }).build();
+    }
+    public OkHttpClient getInstance() {
+        return client;
+    }
+}
